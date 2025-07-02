1cf1bbf330948301f6539cc0ae901d68433f8639
Tariq <164895279+TariqGhr1@users.noreply.github.com>
2025-07-02 20:22:39 -0300

    aula1

--- script.js
+++ script.js
@@ -18,18 +18,25 @@ function processaTexto(texto) {
     let palavras = texto.split("/\p{L}+/u/") 
     /* \P negação; {L} conjunto de letras; + uma ou mais ocorrencias; u Unicode*/
 
-    let frequencias = [];
+    const frequencias = contaFrequencias(palavras);
+    
+    let ordenadas = Objeto.keys(frequencias).sort(OrdenaPalavra);
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
 
+    }
+
+    function contaFrequencias(palavras) { 
+    let frequencias = {};
     for (let i in palavras) {
         frequencias[i] = 0;
-        for (let j in palavras) {
-            if palavras[i] == palavras [j]) {
+        for (let j of palavras) {
+            if (i == j) {
                 frequencias[i]++;
             }
         }
     }
+    }
 
-    console.log(frequencias);
-
-    return palavras
-}
+    return palavras;
